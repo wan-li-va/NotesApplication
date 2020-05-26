569a4a2f5e6cc0f10741e3af48a1f99283fae004
Wan Li <wl9wgc@virginia.edu>
2020-05-26 11:53:03 -0400

    Updates

---
+++ precache-manifest.3d83fd897c3571dd421d3c2b388f42a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855e80a769e79b7a7ce46a48077ef579",
+    "url": "/NotesApplication/index.html"
+  },
+  {
+    "revision": "7a090ff47a365c036f8a",
+    "url": "/NotesApplication/static/css/main.dcea6e0b.chunk.css"
+  },
+  {
+    "revision": "c06a0b97f056d6f9472a",
+    "url": "/NotesApplication/static/js/2.0f4dd930.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/NotesApplication/static/js/2.0f4dd930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a090ff47a365c036f8a",
+    "url": "/NotesApplication/static/js/main.9bf76180.chunk.js"
+  },
+  {
+    "revision": "36552807f93be8f37890",
+    "url": "/NotesApplication/static/js/runtime-main.e415a838.js"
+  }
+]);
